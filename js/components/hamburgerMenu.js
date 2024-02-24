d484a161cebc57b907e9e5234a7d9d46a6bd6e8b
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-02-24 13:30:10 +0100

    Adding more to hamburger.menu
    working on animation of the mobile nav.
    Also added new icons to contact

--- js/components/hamburgerMenu.js
+++ js/components/hamburgerMenu.js
@@ -1,10 +1,13 @@
 const nav = document.querySelector(".main-menu");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
+const closeMenu = document.querySelector(".close-menu-icon");
+const openMenu = document.querySelector(".menu-icon");
 
 hamburgerMenu.addEventListener("click", hamburgerMenuClick);
 
 export function hamburgerMenuClick() {
     nav.classList.toggle("active");
+    closeMenu.style.display = "flex";
     let expandedMenu = hamburgerMenu.getAttribute('aria-expanded') === 'true' || false;
     this.setAttribute('aria-expanded', !expandedMenu);
 }
