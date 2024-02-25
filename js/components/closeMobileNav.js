2515e7a1b69d1feacfdd10302c9f583220967e14
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-02-25 12:38:21 +0100

    closeMobileNav fix, css file cleanup
    Noticed a fault in my closeMobileNav function. Now fixed so the hamburger Menu icon changes according to its open or close state.
    cleaned a bit in my css files

--- js/components/closeMobileNav.js
+++ js/components/closeMobileNav.js
@@ -2,11 +2,22 @@ const nav = document.querySelector(".nav-content ul");
 const navA = document.querySelector(".main-menu")
 const main = document.querySelector("main");
 const header = document.querySelector("header");
+const closeMenu = document.querySelector(".close-menu-icon");
+const openMenu = document.querySelector(".menu-icon");
+const logo = document.querySelector(".logo a");
 
 header.addEventListener("click", mobileNavLinkClick);
 main.addEventListener("click", mobileNavLinkClick);
 navA.addEventListener("click", mobileNavLinkClick);
+logo.addEventListener("click", mobileNavLinkClick);
 
 export function mobileNavLinkClick() {
     nav.classList.remove("active");
+    if(openMenu.style.display === "none") {
+        openMenu.style.display = "flex";
+        closeMenu.style.display = "none";
+} else {
+    openMenu.style.display = "none";
+    closeMenu.style.display = "flex";
+}
 }
