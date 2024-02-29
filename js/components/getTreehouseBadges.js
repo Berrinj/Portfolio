4ce101e39254cba48804dfe978fd11c0d1908fa9
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-02-29 23:26:15 +0100

    treehouse stats & updated styling
    Added treehouse stats and styled them.
    played around with styling of the Skills section.
    Added a faint background image to header on desktop and tablet.

--- js/components/getTreehouseBadges.js
+++ js/components/getTreehouseBadges.js
@@ -1,13 +1,26 @@
 const treehouseAPI = "https://teamtreehouse.com/profiles/martelilleberre" + ".json";
-const treehouseBadgesListItem = document.querySelector(".treehouse-badges");
+const treehouseBadgesListItem = document.querySelector(".badges");
+const treehouseBadgesLastAchieved = document.querySelector(".last-achieved");
+const errorMessage = document.querySelector(".stats")
+
 
 export async function getBadges() {
     try {
  const response = await fetch(treehouseAPI);
  const result = await response.json(); 
- treehouseBadgesListItem.innerHTML += result.badges.length;
- console.log(result.badges.length);
+
+    const lastIndex = result.badges.length - 1;
+
+ const earnedDate = new Date(result.badges[lastIndex].earned_date).toLocaleDateString('nb-NO', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+});
+
+ treehouseBadgesListItem.innerHTML = `<b>${result.badges.length}</b>`;
+ treehouseBadgesLastAchieved.innerHTML += `<br>${earnedDate}`;
 } catch(error) {
+    errorMessage.innerHTML = "<li>Klarte ikke hente data fra treehouse API</li>";
     console.error(error);
 }
 }
--- js/components/getTreehousePoints.js
+++ js/components/getTreehousePoints.js
@@ -1,12 +1,15 @@
 const treehouseAPI = "https://teamtreehouse.com/profiles/martelilleberre" + ".json";
 const treehousePointsListItem = document.querySelector(".treehouse-points");
+const treehouseJsPoints = document.querySelector(".treehouse-js-points");
 
 export async function getPoints() {
     try {
  const response = await fetch(treehouseAPI);
  const result = await response.json(); 
- treehousePointsListItem.innerHTML += result.points.total;
+ treehousePointsListItem.innerHTML += `<b>${result.points.total}</b>`;
+ treehouseJsPoints.innerHTML += `<b>${result.points.JavaScript}</b>`;
 } catch(error) {
+    treehousePointsListItem.innerHTML += "Klarte ikke hente poeng data fra treehouse API.";
     console.error(error);
 }
 }
